1344ac6ca213551c83affa29116b2b060ee3b8b2
ntran89 <ntran89@go.pasadena.edu>
2024-01-30 20:09:44 -0800

    Added binary set populator

--- src/main/java/edu/pasadena/cs/cs03b/Dummy.java
+++ src/main/java/edu/pasadena/cs/cs03b/Dummy.java
@@ -1,9 +1,29 @@
 package edu.pasadena.cs.cs03b;
+import java.lang.Math;
 
 public class Dummy {
 
 	public static void main(String[] args) {
 		// TODO: add you logic here
+		int setarr[][] = new int[8][128];
+		// Generate sets
+		for (int i = 0; i < 8; i++) {
+			int index = 0;
+			for (int j = 0; j <= 256; j++) {
+				if ((j & (int) Math.pow(2, i)) != 0) {
+					setarr[i][index] = j;
+					index++;
+				}
+			}
+		}
+		// Print sets
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 128; j++) {
+				System.out.print(setarr[i][j] + " ");
+			}
+			System.out.println();
+			System.out.println();
+		}
 	}
 
 	public static int dummy() {
